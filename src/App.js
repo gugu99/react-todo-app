053026394c5b8b06b85413486c46bc9aceb95108
gugu99 <gde1002@naver.com>
2023-11-16 09:23:11 +0900

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import { Button, Col, Input, Row } from 'antd';
+import axios from 'axios';
 
 function App() {
+  const [id, setId] = useState();
+  const [pw, setPw] = useState();
+
+  const onchangeId = (e) => {
+    const id = e.target.value;
+    setId(id);
+  };
+
+  const onchagePw = (e) => {
+    const pw = e.target.value;
+    setPw(pw);
+  }
+
+  const submit = async () => {
+    const data = {username: id, password: pw}
+
+    const result = (await (axios.post("http://localhost:8080/api/login", data))).data;
+    console.log("result", result);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Row>
+      <Col md={8}>
+        <Input placeholder="아이디"  onChange={onchangeId}/>
+        <Input style={{marginTop:"1rem"}} type="password" placeholder="비밀번호"  onChange={onchagePw}/>
+        <Button style={{float:"right", marginTop:"1rem"}} type="primary" onClick={submit}>로그인</Button>
+      </Col>
+    </Row>
   );
 }
 
